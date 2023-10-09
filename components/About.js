1de842eef9648e4d91856ba45fd24d701ee67d60
Seth Kuhn <107776853+SethK329@users.noreply.github.com>
2023-10-08 22:53:07 -0700

    first commit

---
+++ components/About.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default function About(){
+    return (
+        <section className = "about">
+            <h3>About</h3>
+            <p>I am a husband and a father of two young children. I am working to pivot careers into tech, not only to be able to better provide, but also pursue a career that I will find fulfillment in. Having worked as a pest control technician for 10 years, I have extensive knowledge in identifying and eliminating bugs. My analytical and problem solving skills would be useful in any codebase.
+            </p>
+        </section>
+    )
+}
---
+++ components/Interests.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default function Interests(){
+    return(
+        <section className = "interests">
+            <h3>Interests</h3>
+            <p>I enjoy spending time with my family the most. When I do have time to myself I build a variety of things, usually with wood. I have built furniture and mallets and a toddler slide and of course a cutting board. I find being able to make things that get used and have a purpose is immensely satisfying. The joy and satisfaction I get from building something that is functional, beautiful, and refined, out of wood is what I feel when building with code too.</p>
+        </section>
+    )
+}
